3f7340bcaa34b855e5cca64304f957c5bc8d61f4
Julie Gankina <qunya11@gmail.com>
2020-06-12 20:21:05 +0300

    adds PropTypes, sets propTypes for props

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom";
 
 import App from "./components/app/app.jsx";
 
-import {PromoData} from "./consts.js";
+import {PromoData, TITLES} from "./consts.js";
 
 ReactDOM.render(
     <App
       movieName = {PromoData.movieName}
       movieGenre = {PromoData.movieGenre}
       movieReleaseDate = {PromoData.movieReleaseDate}
+      movieTitles = {TITLES}
     />,
     document.querySelector(`#root`)
 );
