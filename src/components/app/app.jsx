fead58f6b644eff0c4032018f0fc825e83e7b16c
Julie Gankina <qunya11@gmail.com>
2020-07-04 20:06:42 +0300

    add router to App page

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,21 +1,47 @@
-import React from "react";
+import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 import Main from "../main/main.jsx";
+import MoviePage from "../movie-page/movie-page.jsx";
 
 const titleClickHandler = () => {};
 
-const App = ({movieName, movieGenre, movieReleaseDate, movies}) => {
-  return (
-    <Main
-      movieName = {movieName}
-      movieGenre = {movieGenre}
-      movieReleaseDate = {movieReleaseDate}
-      onTitleClick = {titleClickHandler}
-      movies = {movies}
-    />
-  );
-};
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  _renderApp() {
+    const {movieName, movieGenre, movieReleaseDate, movies} = this.props;
+
+    return (
+      <Main
+        movieName = {movieName}
+        movieGenre = {movieGenre}
+        movieReleaseDate = {movieReleaseDate}
+        onTitleClick = {titleClickHandler}
+        movies = {movies}
+      />
+    );
+  }
+
+  render() {
+
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            {this._renderApp()}
+          </Route>
+          <Route exact path="/dev-component">
+            <MoviePage />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 App.propTypes = {
   movieName: PropTypes.string.isRequired,
