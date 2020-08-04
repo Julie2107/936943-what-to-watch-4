ab3290968f9ce9d8e10ea5f7bf3c35a4d3a4735b
Julie Gankina <qunya11@gmail.com>
2020-08-04 23:40:32 +0300

    corrections for 6.2 task

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -13,7 +13,18 @@ import {getGenres, getPromoMovie} from "../../reducer/data/selectors.js";
 
 const WrappedFullscreen = withFullScreenVideo(FullScreenVideo);
 
-const Main = ({promoMovie, movies, genresList, onFilterChange, currentGenre, onButtonClick, shownMoviesNumber, onTitleClick, onActivatePlayer, onDeactivatePlayer, isActivePlayer}) => {
+const Main = ({
+  promoMovie,
+  movies,
+  genresList,
+  onFilterChange,
+  currentGenre,
+  onButtonClick,
+  shownMoviesNumber,
+  onTitleClick,
+  onActivatePlayer,
+  onDeactivatePlayer,
+  isActivePlayer}) => {
 
   const moviesToRender = movies.slice(0, shownMoviesNumber);
   const isButtonToRender = shownMoviesNumber < movies.length ? <ShowMoreButton
@@ -21,98 +32,98 @@ const Main = ({promoMovie, movies, genresList, onFilterChange, currentGenre, onB
   /> : ``;
 
   return (
-    isActivePlayer ? (<WrappedFullscreen className="player__video"
-      movie={promoMovie}
-      onDeactivatePlayer={onDeactivatePlayer}
-    />) : (
-    <>
-      <section className="movie-card">
-        <div className="movie-card__bg">
-          <img src={promoMovie.backgroundImage} alt={promoMovie.title} />
-        </div>
-
-        <h1 className="visually-hidden">WTW</h1>
-
-        <header className="page-header movie-card__head">
-          <div className="logo">
-            <a className="logo__link">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </a>
+    isActivePlayer ?
+      (<WrappedFullscreen className="player__video"
+        movie={promoMovie}
+        onDeactivatePlayer={onDeactivatePlayer}
+      />) :
+      (<>
+        <section className="movie-card">
+          <div className="movie-card__bg">
+            <img src={promoMovie.backgroundImage} alt={promoMovie.title} />
           </div>
 
-          <div className="user-block">
-            <div className="user-block__avatar">
-              <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
-            </div>
-          </div>
-        </header>
+          <h1 className="visually-hidden">WTW</h1>
 
-        <div className="movie-card__wrap">
-          <div className="movie-card__info">
-            <div className="movie-card__poster">
-              <img src={promoMovie.poster} alt={`${promoMovie.title} poster`} width="218" height="327" />
+          <header className="page-header movie-card__head">
+            <div className="logo">
+              <a className="logo__link">
+                <span className="logo__letter logo__letter--1">W</span>
+                <span className="logo__letter logo__letter--2">T</span>
+                <span className="logo__letter logo__letter--3">W</span>
+              </a>
             </div>
 
-            <div className="movie-card__desc">
-              <h2 className="movie-card__title">{promoMovie.title}</h2>
-              <p className="movie-card__meta">
-                <span className="movie-card__genre">{promoMovie.genre}</span>
-                <span className="movie-card__year">{promoMovie.releaseYear}</span>
-              </p>
+            <div className="user-block">
+              <div className="user-block__avatar">
+                <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
+              </div>
+            </div>
+          </header>
 
-              <div className="movie-card__buttons">
-                <button className="btn btn--play movie-card__button" type="button"
-                  onClick={onActivatePlayer}>
-                  <svg viewBox="0 0 19 19" width="19" height="19">
-                    <use xlinkHref="#play-s" />
-                  </svg>
-                  <span>Play</span>
-                </button>
-                <button className="btn btn--list movie-card__button" type="button">
-                  <svg viewBox="0 0 19 20" width="19" height="20">
-                    <use xlinkHref="#add" />
-                  </svg>
-                  <span>My list</span>
-                </button>
+          <div className="movie-card__wrap">
+            <div className="movie-card__info">
+              <div className="movie-card__poster">
+                <img src={promoMovie.poster} alt={`${promoMovie.title} poster`} width="218" height="327" />
+              </div>
+
+              <div className="movie-card__desc">
+                <h2 className="movie-card__title">{promoMovie.title}</h2>
+                <p className="movie-card__meta">
+                  <span className="movie-card__genre">{promoMovie.genre}</span>
+                  <span className="movie-card__year">{promoMovie.releaseYear}</span>
+                </p>
+
+                <div className="movie-card__buttons">
+                  <button className="btn btn--play movie-card__button" type="button"
+                    onClick={onActivatePlayer}>
+                    <svg viewBox="0 0 19 19" width="19" height="19">
+                      <use xlinkHref="#play-s" />
+                    </svg>
+                    <span>Play</span>
+                  </button>
+                  <button className="btn btn--list movie-card__button" type="button">
+                    <svg viewBox="0 0 19 20" width="19" height="20">
+                      <use xlinkHref="#add" />
+                    </svg>
+                    <span>My list</span>
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
-
-      <div className="page-content">
-        <section className="catalog">
-          <h2 className="catalog__title visually-hidden">Catalog</h2>
-          <GenresList
-            genresList={genresList}
-            onFilterChange={onFilterChange}
-            currentGenre={currentGenre}
-          />
-          <MoviesList
-            movies={moviesToRender}
-            onTitleClick={onTitleClick}
-          />
-          {isButtonToRender}
         </section>
 
-        <footer className="page-footer">
-          <div className="logo">
-            <a className="logo__link logo__link--light">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </a>
-          </div>
+        <div className="page-content">
+          <section className="catalog">
+            <h2 className="catalog__title visually-hidden">Catalog</h2>
+            <GenresList
+              genresList={genresList}
+              onFilterChange={onFilterChange}
+              currentGenre={currentGenre}
+            />
+            <MoviesList
+              movies={moviesToRender}
+              onTitleClick={onTitleClick}
+            />
+            {isButtonToRender}
+          </section>
 
-          <div className="copyright">
-            <p>© 2019 What to watch Ltd.</p>
-          </div>
-        </footer>
-      </div>
-    </>
-    )
+          <footer className="page-footer">
+            <div className="logo">
+              <a className="logo__link logo__link--light">
+                <span className="logo__letter logo__letter--1">W</span>
+                <span className="logo__letter logo__letter--2">T</span>
+                <span className="logo__letter logo__letter--3">W</span>
+              </a>
+            </div>
+
+            <div className="copyright">
+              <p>© 2019 What to watch Ltd.</p>
+            </div>
+          </footer>
+        </div>
+      </>)
   );
 };
 
--- src/components/movie-page/movie-page.jsx
+++ src/components/movie-page/movie-page.jsx
@@ -24,89 +24,89 @@ const getSimilarMovies = (movies, movie) => {
 
 const MoviePage = ({movies, movie, onTitleClick, onActivatePlayer, onDeactivatePlayer, isActivePlayer}) => {
   return (
-    isActivePlayer ? (<WrappedFullscreen className="player__video"
-      movie={movie}
-      onDeactivatePlayer={onDeactivatePlayer}
-    />) : (
-    <>
-      <section className="movie-card movie-card--full">
-        <div className="movie-card__hero">
-          <div className="movie-card__bg" style={{backgroundColor: movie.backgroundColor}}>
-            <img src={movie.backgroundImage} alt={movie.title}/>
-          </div>
-
-          <h1 className="visually-hidden">WTW</h1>
-
-          <header className="page-header movie-card__head">
-            <div className="logo">
-              <a href="main.html" className="logo__link">
-                <span className="logo__letter logo__letter--1">W</span>
-                <span className="logo__letter logo__letter--2">T</span>
-                <span className="logo__letter logo__letter--3">W</span>
-              </a>
+    isActivePlayer ?
+      (<WrappedFullscreen className="player__video"
+        movie={movie}
+        onDeactivatePlayer={onDeactivatePlayer}
+      />) :
+      (<>
+        <section className="movie-card movie-card--full">
+          <div className="movie-card__hero">
+            <div className="movie-card__bg" style={{backgroundColor: movie.backgroundColor}}>
+              <img src={movie.backgroundImage} alt={movie.title}/>
             </div>
 
-            <div className="user-block">
-              <div className="user-block__avatar">
-                <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
+            <h1 className="visually-hidden">WTW</h1>
+
+            <header className="page-header movie-card__head">
+              <div className="logo">
+                <a href="main.html" className="logo__link">
+                  <span className="logo__letter logo__letter--1">W</span>
+                  <span className="logo__letter logo__letter--2">T</span>
+                  <span className="logo__letter logo__letter--3">W</span>
+                </a>
+              </div>
+
+              <div className="user-block">
+                <div className="user-block__avatar">
+                  <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
+                </div>
+              </div>
+            </header>
+
+            <div className="movie-card__wrap">
+              <div className="movie-card__desc">
+                <h2 className="movie-card__title">{movie.title}</h2>
+                <p className="movie-card__meta">
+                  <span className="movie-card__genre">{movie.genre}</span>
+                  <span className="movie-card__year">{movie.releaseYear}</span>
+                </p>
+
+                <div className="movie-card__buttons">
+                  <button className="btn btn--play movie-card__button" type="button"
+                    onClick={onActivatePlayer}>
+                    <svg viewBox="0 0 19 19" width="19" height="19">
+                      <use xlinkHref="#play-s"></use>
+                    </svg>
+                    <span>Play</span>
+                  </button>
+                  <button className="btn btn--list movie-card__button" type="button">
+                    <svg viewBox="0 0 19 20" width="19" height="20">
+                      <use xlinkHref="#add" />
+                    </svg>
+                    <span>My list</span>
+                  </button>
+                  <a href="add-review.html" className="btn movie-card__button">Add review</a>
+                </div>
               </div>
             </div>
-          </header>
+          </div>
 
-          <div className="movie-card__wrap">
-            <div className="movie-card__desc">
-              <h2 className="movie-card__title">{movie.title}</h2>
-              <p className="movie-card__meta">
-                <span className="movie-card__genre">{movie.genre}</span>
-                <span className="movie-card__year">{movie.releaseYear}</span>
-              </p>
-
-              <div className="movie-card__buttons">
-                <button className="btn btn--play movie-card__button" type="button"
-                  onClick={onActivatePlayer}>
-                  <svg viewBox="0 0 19 19" width="19" height="19">
-                    <use xlinkHref="#play-s"></use>
-                  </svg>
-                  <span>Play</span>
-                </button>
-                <button className="btn btn--list movie-card__button" type="button">
-                  <svg viewBox="0 0 19 20" width="19" height="20">
-                    <use xlinkHref="#add" />
-                  </svg>
-                  <span>My list</span>
-                </button>
-                <a href="add-review.html" className="btn movie-card__button">Add review</a>
+          <div className="movie-card__wrap movie-card__translate-top">
+            <div className="movie-card__info">
+              <div className="movie-card__poster movie-card__poster--big">
+                <img src={movie.poster} alt={movie.title} width="218" height="327" />
               </div>
+
+              <WrappedTabs
+                movie={movie}
+              />
+
             </div>
           </div>
-        </div>
-
-        <div className="movie-card__wrap movie-card__translate-top">
-          <div className="movie-card__info">
-            <div className="movie-card__poster movie-card__poster--big">
-              <img src={movie.poster} alt={movie.title} width="218" height="327" />
-            </div>
-
-            <WrappedTabs
-              movie={movie}
-            />
-
-          </div>
-        </div>
-      </section>
-
-      <div className="page-content">
-        <section className="catalog catalog--like-this">
-          <h2 className="catalog__title">More like this</h2>
-
-          <MoviesList
-            movies={getSimilarMovies(movies, movie)}
-            onTitleClick={onTitleClick}
-          />
         </section>
-      </div>
-    </>
-    )
+
+        <div className="page-content">
+          <section className="catalog catalog--like-this">
+            <h2 className="catalog__title">More like this</h2>
+
+            <MoviesList
+              movies={getSimilarMovies(movies, movie)}
+              onTitleClick={onTitleClick}
+            />
+          </section>
+        </div>
+      </>)
   );
 };
 
